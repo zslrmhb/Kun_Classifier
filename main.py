df7b1a9ac8e03f0021ccba8181d7bfcd3d9f1332
Hongbin Miao <zslrmhb@gmail.com>
2022-11-11 23:59:13 -0800

    1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 import os
 import cv2
@@ -10,6 +11,36 @@ import  matplotlib.pyplot as plt
 from torchvision import transforms, models
 from PIL import Image, ImageFont, ImageDraw
 
+def autoplay_audio(file_path: str, loop=False):
+  # code from https://github.com/streamlit/streamlit/issues/2446
+  if loop:
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true" loop="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.empty()
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+  else:
+    with open(file_path, "rb") as f:
+      data = f.read()
+      b64 = base64.b64encode(data).decode()
+      md = f"""
+          <audio controls autoplay="true">
+          <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+          </audio>
+          """
+      st.empty()
+      st.markdown(
+          md,
+          unsafe_allow_html=True,
+        )
 
 KUN_DIR = os.getcwd() + '/Dataset/kun'
 CHICKEN_DIR = os.getcwd() + '/Dataset/zhiyin'
@@ -34,8 +65,8 @@ class Kun_Classifier:
     :params: img_path: the image path of the image for inference
     :returns: the inference result in terms of strings
     """
-    kun = "KUN | 含坤量为:{}%"
-    chicken = "CHICKEN | 含只因量为:{}%"
+    kun = "KUN: {a}% | 含坤量为:{a}%"
+    chicken = "CHICKEN: {a}% | 含只因量为:{a}%"
 
     model = models.resnet18(pretrained=True)
     nr_filters = model.fc.in_features
@@ -59,34 +90,20 @@ class Kun_Classifier:
     sigmoid = torch.sigmoid(pred)
 
     if sigmoid < 0.5:
-      return kun.format(round(float(100 * (1-sigmoid)), 2))
+      autoplay_audio('biebie.wav')
+      return kun.format(a=round(float(100 * (1-sigmoid)), 2))
     else:
-      return chicken.format(round(float(100 * sigmoid), 2))
+      autoplay_audio('zhiyin.wav')
+      return chicken.format(a=round(float(100 * sigmoid), 2))
 
 kuner = Kun_Classifier()
 
 # Welcome Page
 st.title("KUN-er Classifier")
+autoplay_audio('ji.mp3')
 st.caption('Welcome! | 欢迎各位小黑子们前来体验二元坤类器! | https://github.com/zslrmhb/Kun_Classifier')
-st.text("""
 
-    """)
 
-def autoplay_audio(file_path: str):
-  # code from https://github.com/streamlit/streamlit/issues/2446
-    with open(file_path, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
-        md = f"""
-            <audio controls autoplay="true" loop="true">
-            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-            </audio>
-            """
-        st.empty()
-        st.markdown(
-            md,
-            unsafe_allow_html=True,
-        )
 if st.button("Don't Click! | 小黑子勿按！"):autoplay_audio("background.mp3")
 
 
@@ -98,9 +115,9 @@ with tab1:
 
   def show_and_classify(user_choice, classify=False):
 
-      if user_choice == 0:
+      if user_choice == 'kun | 坤':
           result = KUN_DIR + '/' + random.choice(os.listdir(KUN_DIR))
-      elif user_choice == 1:
+      elif user_choice == 'chicken | 只因':
           result = CHICKEN_DIR + '/' + random.choice(os.listdir(CHICKEN_DIR))
       else:
           result = random.choice([KUN_DIR + '/' + random.choice(os.listdir(KUN_DIR)), CHICKEN_DIR + '/' + random.choice(os.listdir(CHICKEN_DIR))])
@@ -111,13 +128,14 @@ with tab1:
 
       if classify: st.caption(kuner.inference(result))
 
-  desired_image = st.slider('Kun(0) or Chicken(1)? | 坤 或 只因', 0, 1)
+
+  desired_image = st.select_slider('', options=['kun | 坤', 'chicken | 只因'])
   if (st.button("Get")): show_and_classify(desired_image)
 
 with tab2:
 # Classification
   st.subheader('Classification | 二元坤类器')
-  desired_classify = st.slider('Kun(0) or Chicken(1) or Random(2)? | 坤 或 只因 或 两个都要 (bushi', 0, 2, 1)
+  desired_classify = st.select_slider('', options=['kun | 坤',  'Random | 随便', 'chicken | 只因'])
   if (st.button("Classify")): show_and_classify(desired_classify, True)
 
 with tab3:
